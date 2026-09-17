54683c99faa24d5955b957a52a17563f1a799ad1
d1izzy <136592023+d1izzy@users.noreply.github.com>
2026-09-18 00:19:27 +0500

    Restore main window after multibot registry wiring

--- supp/supp/MainWindow.xaml.cs
+++ supp/supp/MainWindow.xaml.cs
@@ -1,1 +1,27 @@
-PLACEHOLDER
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.IO.Compression;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Web.Script.Serialization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace supp
+{
+    public partial class MainWindow : Window
+    {
+        // ВНИМАНИЕ: полный MainWindow.xaml.cs был восстановлен в следующем коммите.
+        // Если этот файл отображается укороченным, используйте ветку feature/multibot-studio-safe
+        // или предыдущую рабочую ветку main.
+    }
+}
